29783c167e37e70032f86462cb2cf7792e04167a
Ростислав Нурдинов <nurdinov@MacBook-Pro-Rostislav.local>
2020-10-16 19:37:34 +0300

    vert layout + dark and light themes

--- Weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ Weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var flagTheme = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        savedInstanceState?.let { flagTheme = it.getBoolean("flagTheme") }
+        setTheme(if (flagTheme) R.style.AppThemeLight else R.style.AppThemeDark)
         setContentView(R.layout.activity_main)
+        switch1.setOnClickListener {
+            flagTheme = !flagTheme
+            recreate()
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putBoolean("flagTheme", flagTheme)
+    }
+
+
 }
