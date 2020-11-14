38346a95ef179d115762f3a82e2eed737af42810
NurdinovRost <nurdinov.rost@gmail.com>
2020-11-14 08:37:21 +0300

    added api + horizontal layout

--- Weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ Weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.weather
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.moshi.Json
 import com.squareup.moshi.Moshi
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,13 +16,14 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import java.lang.Math.round
+import kotlin.math.roundToInt
 
 
 class WeatherResponse(
-    @field:Json(name = "lat") var lat: Float,
-    @field:Json(name = "lon") var lon: Float,
     @field:Json(name = "timezone") var timezone: String,
     @field:Json(name = "current") var current: Current,
+    @field:Json(name = "daily") var daily: List<Dialy>,
 )
 
 class Current(
@@ -27,6 +31,21 @@ class Current(
     @field:Json(name = "temp") var temp: Float,
     @field:Json(name = "clouds") var clouds: Int,
     @field:Json(name = "wind_speed") var wind_speed: Int,
+    @field:Json(name = "weather") var weather: List<WeatherDescription>,
+)
+
+class Dialy(
+    @field:Json(name = "temp") var temp: Temp,
+    @field:Json(name = "weather") var weather: List<WeatherDescription>,
+)
+
+class Temp(
+    @field:Json(name = "day") var day: Float,
+)
+
+class WeatherDescription(
+    @field:Json(name = "main") var main: String,
+    @field:Json(name = "icon") var icon: String,
 )
 
 
@@ -42,7 +61,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     public interface OpenWeatherMapService {
-        @GET("onecall?lat=59.937500&lon=30.308611&exclude=minutely,hourly,daily,alerts&units=imperial&appid=508af3c89d5fdcb4b9f030cea26e964e")
+        @GET("onecall?lat=59.937500&lon=30.308611&exclude=minutely,hourly,alerts&units=metric&appid=508af3c89d5fdcb4b9f030cea26e964e")
         fun listWeather(): Call<WeatherResponse>
     }
 
@@ -67,25 +86,55 @@ class MainActivity : AppCompatActivity() {
         val service = retrofit.create(OpenWeatherMapService::class.java)
         val call: Call<WeatherResponse> = service.listWeather()
         call.enqueue(object : Callback<WeatherResponse> {
+            @SuppressLint("SetTextI18n")
             override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
                 if (response.code() == 200) {
                     val weatherResponse = response.body()!!
 
                     val stringBuilder = "lat: " +
-                            weatherResponse.lat.toString() +
-                            "\n" +
-                            "temp: " +
                             weatherResponse.current.temp.toString() +
                             "\n" +
-                            "timezone" +
-                            weatherResponse.timezone
+//                            "timezone" +
+//                            weatherResponse.weather[0].main
 
-                    Log.d("MSG-ff", stringBuilder)
-                    textView.text = weatherResponse.current.temp.toString()
-                    textView2.text = weatherResponse.timezone
+                    Log.d("MSG-ff", "${weatherResponse.daily[0].temp}")
+                    val t = weatherResponse.current.temp.roundToInt()
+                    if (t > 0) {
+                        textView.text = "${t}°C"
+                    } else {
+                        textView.text = "-${t}°C"
+                    }
+                    textView2.text = weatherResponse.timezone + "\n" + weatherResponse.current.weather[0].main
                     textViewWindFLow.text = weatherResponse.current.wind_speed.toString()
                     textViewCloudy.text = weatherResponse.current.clouds.toString()
                     textViewHumidity.text = weatherResponse.current.humidity.toString()
+                    val icon1 = weatherResponse.current.weather[0].icon
+                    Picasso.get()
+                        .load("https://openweathermap.org/img/wn/$icon1@2x.png")
+                        .fit().centerCrop()
+                        .into(icon)
+                    fun genPredictionOnEveryDay(days: List<Dialy>) {
+                        val imgDay = listOf(imgPN, imgVT, imgSR, imgCT, imgPT, imgSB, imgVS)
+                        val textViewTempDay = listOf(textViewTempPN, textViewTempVT, textViewTempSR, textViewTempCT, textViewTempPT, textViewTempSB, textViewTempVS)
+                        days.forEachIndexed { index, day ->
+                            Log.d("MSGHELLO", "HELLO")
+                            if (index != 0) {
+                                val tempNow = day.temp.day.roundToInt()
+                                if (tempNow >= 0) {
+                                    textViewTempDay[index - 1].text = "+$tempNow"
+                                } else {
+                                    textViewTempDay[index - 1].text = "$tempNow"
+                                }
+                                val iconPath = day.weather[0].icon
+                                Picasso.get()
+                                    .load("https://openweathermap.org/img/wn/$iconPath@2x.png")
+                                    .fit().centerCrop()
+                                    .into(imgDay[index - 1])
+                            }
+                        }
+
+                    }
+                    genPredictionOnEveryDay(weatherResponse.daily)
                 }
             }
 
